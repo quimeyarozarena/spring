6fd2ad8e028f9d1f269378a48154aef0a399957c
Quimey Arozarena <quimeyarozarena@gmail.com>
2016-01-22 17:36:00 -0300

    add sign up

--- Spring_Web_App/WebContent/WEB-INF/jsp/signup.jsp
+++ Spring_Web_App/WebContent/WEB-INF/jsp/signup.jsp
@@ -8,5 +8,61 @@
 </head>
 <body>
 
+<div>
+
+<label>${message}</label>
+
+	<form action="${pageContext.request.contextPath}/signup" method="post" enctype="multipart/form-data">
+		<table>
+			<tr>
+				<td><label>Enter username: </label></td>
+				<td><input type="text" name="username"/></td>
+			</tr>
+			
+			<tr>
+				<td><label>Enter password: </label></td>
+				<td><input type="password" name="password"/></td>
+			</tr>
+			
+			<tr>
+				<td><label>Re-enter password: </label></td>
+				<td><input type="password" name="repassword"/></td>
+			</tr>
+			
+			<tr>
+				<td><label>Choose Gender: </label></td>
+				<td><input type="radio" name="gender" value="male"/>
+				<input type="radio" name="gender" value="female"/></td>
+			</tr>
+			
+			<tr>
+				<td><label>Select vehicle: </label></td>
+				<td><input type="checkbox" name="vehicle" value="Car"/>
+				<input type="checkbox" name="vehicle" value="Bike"/></td>
+			</tr>
+			
+			<tr>
+			
+			<td><label>Select country: </label></td>
+			
+				<td>
+					<select name="country">
+						<option value="India">INDIA</option>
+						<option value="usa">USA</option>
+						<option value="uk">UK</option>
+					</select>
+				</td>
+			</tr>
+			
+			<tr>
+				<td><label>Select image: </label></td>
+				<td><input type="file" name = "image"/></td>
+			</tr>
+		</table>
+	
+	<input type="submit" value="Save" />
+	</form>
+</div>
+
 </body>
 </html>
--- Spring_Web_App/src/com/demo/controllers/Signup_Controller.java
+++ Spring_Web_App/src/com/demo/controllers/Signup_Controller.java
@@ -1,5 +1,12 @@
 package com.demo.controllers;
 
+import com.demo.models.Signup_Model;
+import java.io.File;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +20,56 @@ public class Signup_Controller {
 
         ModelAndView mav = new ModelAndView("signup");
 
+        return mav;
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public ModelAndView doSignUpProcess(HttpServletRequest request) {
+
+        String message = null;
+        ModelAndView mav = new ModelAndView("signup");
+        /*
+         * String username = request.getParameter("username"); String password = request.getParameter("password");
+         */
+
+        // no se hace como arriba por este atributo en el form: enctype="multipart/form-data"
+
+        if (ServletFileUpload.isMultipartContent(request)) {
+
+            try {
+                List<FileItem> data = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+
+                String username = data.get(0).getString();
+                String password = data.get(1).getString();
+                String repassword = data.get(2).getString();
+                String gender = data.get(3).getString();
+                String vehicle = data.get(4).getString();
+                String country = data.get(5).getString();
+
+                String image = new File(data.get(6).getName()).getName();
+
+                if (password.equals(repassword)) {
+
+                    Signup_Model sm = new Signup_Model();
+                    sm.doSignUp(username, repassword, gender, vehicle, country, image);
+
+                    // the first part redirect to project and the second part go to images inside web-inf
+                    String path = request.getSession().getServletContext().getRealPath("/") + "//WEB-INF//images//";
+
+                    data.get(6).write(new File(path + File.separator + image));
+
+                    System.out.print(path);
+                } else {
+                    message = "Password doesn't match.. please try again";
+                }
+
+            } catch (Exception e) {
+
+                message = "please try again...";
+            }
+        }
+
+        mav.addObject("message", message);
         return mav;
     }
 
